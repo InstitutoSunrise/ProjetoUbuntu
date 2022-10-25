1a017bd645f489b03a106c0ec2601d195450d5d1
w3llz1n <wellintoh.silva.brito@gmail.com>
2022-10-25 10:49:18 -0300

    Adicionando: Sistema de excluir publicação

--- src/components/minhaPublicacao/index.js
+++ src/components/minhaPublicacao/index.js
@@ -1,130 +1,274 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
-import { FontAwesome } from '@expo/vector-icons';
-import CarouselPost from '../CarouselPost';
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  Modal,
+} from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+import CarouselPost from "../CarouselPost";
+import { getStorage, ref, deleteObject } from "firebase/storage";
+import { doc, deleteDoc } from "firebase/firestore";
+import db from "../../config/configFirebase";
 
-export default function MinhaPublicacao({ sobreVoce, tipoAjuda, imgPost1, imgPost2, imgPost3, status, dataHoraPost, navigation }) {
+export default function MinhaPublicacao({
+  sobreVoce,
+  tipoAjuda,
+  imgPost1,
+  imgPost2,
+  imgPost3,
+  status,
+  dataHoraPost,
+  navigation,
+  idPost,
+}) {
+  const [isStatus, setStatus] = useState(status);
+  const [imagesPost, setImagesPost] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
 
-    const [isStatus, setStatus] = useState(status)
-    const [imagesPost, setImagesPost] = useState([])
+  const deletarPost = async () => {
+    const storage = getStorage();
 
+    // Create a reference to the file to delete
+    const desertRef = ref(storage, imgPost1);
+    const desertRef2 = ref(storage, imgPost2);
+    const desertRef3 = ref(storage, imgPost3);
 
-    const getImages = () => {
-        let newArray = imagesPost
-        newArray = [imgPost1, imgPost2, imgPost3]
-        setImagesPost([...newArray])
+    if (isStatus == false) {
+      await deleteDoc(doc(db, "posts", idPost));
+      console.log("Deletado com sucesso");
+      setModalVisible(!modalVisible);
+    } else {
+      // Delete the file
+      deleteObject(desertRef)
+        .then(() => {
+          // File deleted successfully
+        })
+        .catch((error) => {
+          // Uh-oh, an error occurred!
+        });
+
+      deleteObject(desertRef2)
+        .then(() => {
+          // File deleted successfully
+        })
+        .catch((error) => {
+          // Uh-oh, an error occurred!
+        });
+
+      deleteObject(desertRef3)
+        .then(() => {
+          // File deleted successfully
+        })
+        .catch((error) => {
+          // Uh-oh, an error occurred!
+        });
+
+      await deleteDoc(doc(db, "posts", idPost));
+      console.log("Deletado com sucesso");
+      setModalVisible(!modalVisible);
     }
+  };
 
-    useEffect(() => {
-        getImages()
-    }, [imgPost3])
+  const getImages = () => {
+    let newArray = imagesPost;
+    newArray = [imgPost1, imgPost2, imgPost3];
+    setImagesPost([...newArray]);
+  };
 
-    return (
-        <View style={styles.card}>
-            <View style={styles.viewInfo}>
-                <Text style={styles.dataPost}>{dataHoraPost}</Text>
+  useEffect(() => {
+    getImages();
+  }, [imgPost3]);
 
-                {isStatus ?
-                    <Text style={styles.status}>Você esta doando</Text>
-                    : <Text style={styles.status}>Você esta Recebendo</Text>}
-
-                <Text style={styles.titulo}>{tipoAjuda}</Text>
-                <Text style={styles.description}>{sobreVoce}</Text>
+  return (
+    <>
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.viewAlert}>
+            <Text style={styles.textAlert}>
+              Tem certeza que vai deletar essa publicação?
+            </Text>
+            <View style={styles.viewBtnAlert}>
+              <TouchableOpacity
+                style={styles.btnAlert}
+                onPress={() => setModalVisible(!modalVisible)}
+              >
+                <Text style={styles.btnTextAlert}>Agora não</Text>
+              </TouchableOpacity>
+              <TouchableOpacity 
+                style={styles.btnAlert} 
+                onPress={deletarPost}
+              >
+                <Text style={styles.btnTextAlert}>Deletar</Text>
+              </TouchableOpacity>
             </View>
-
-            {isStatus ?
-                <View style={styles.boxImages}>
-                    <CarouselPost
-                        data={imagesPost}
-                    />
-                </View>
-                : undefined}
-                
-            <View style={styles.ViewBtn}>
-                <TouchableOpacity style={styles.button}>
-                    <Text style={styles.text}>EXCLUIR</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('EditarPost', { sobreVoce: sobreVoce, tipoAjuda: tipoAjuda })}>
-                    <Text style={styles.text}>EDITAR</Text>
-                </TouchableOpacity>
-            </View>
-
+          </View>
         </View>
-    );
+      </Modal>
+
+      <View style={styles.card}>
+        <View style={styles.viewInfo}>
+          <Text style={styles.dataPost}>{dataHoraPost}</Text>
+
+          {isStatus ? (
+            <Text style={styles.status}>Você esta doando</Text>
+          ) : (
+            <Text style={styles.status}>Você esta Recebendo</Text>
+          )}
+
+          <Text style={styles.titulo}>{tipoAjuda}</Text>
+          <Text style={styles.description}>{sobreVoce}</Text>
+        </View>
+
+        {isStatus ? (
+          <View style={styles.boxImages}>
+            <CarouselPost data={imagesPost} />
+          </View>
+        ) : undefined}
+
+        <View style={styles.ViewBtn}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => setModalVisible(!modalVisible)}
+          >
+            <Text style={styles.text}>EXCLUIR</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() =>
+              navigation.navigate("EditarPost", {
+                sobreVoce: sobreVoce,
+                tipoAjuda: tipoAjuda,
+              })
+            }
+          >
+            <Text style={styles.text}>EDITAR</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
-    card: {
-        backgroundColor: '#e8eaea',
-        height: 'auto',
-        borderRadius: 8,
-        padding: 15,
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        marginVertical: 10,
-        marginHorizontal: 10
-    },
-    viewInfo: {
-        width: '100%'
-    },
-    dataPost: {
-        fontSize: 13,
-        fontWeight: '300',
-        textAlign: 'right',
-        marginVertical: 2,
-    },
-    titulo: {
-        fontSize: 18,
-        fontWeight: '700',
-        textTransform: 'uppercase',
-        marginVertical: 5
-    },
-    description: {
-        width: '100%',
-        marginBottom: 10
-    },
-    fotoPerfil: {
-        width: 50,
-        height: 50,
-        borderRadius: 60,
-        borderWidth: 2,
-        borderColor: '#0e52B2'
-    },
-    ViewBtn: {
-        width: '80%',
-        alignItems: 'center',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-    },
-    button: {
-        width: '40%',
-        backgroundColor: '#38B6FF',
-        borderRadius: 25,
-        padding: 15,
-        alignItems: 'center'
-    },
-    text: {
-        fontSize: 18,
-        color: '#fff',
-        fontWeight: '800',
-        letterSpacing: 1,
-    },
-    status: {
-        color: '#38B6FF',
-        fontWeight: 'bold',
-        fontSize: 18,
-        textDecorationLine: 'underline',
-        textTransform: 'uppercase',
-        marginBottom: 5
-    },
-    boxImages: {
-        width: '100%',
-        padding: 5,
-    },
-    img: {
-        width: 70,
-        height: 70,
-        margin: 10,
-    },
-
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#rgba(0,0,0,0.5)",
+  },
+  viewAlert: {
+    width: "70%",
+    backgroundColor: "#fff",
+    borderRadius: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+  },
+  textAlert: {
+    width: "100%",
+    fontSize: 15,
+    fontWeight:'500',
+    color: "#0e52B2",
+  },
+  viewBtnAlert: {
+    flexDirection: "row",
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "space-around",
+    marginTop:15,
+  },
+  btnAlert: {
+    width: '40%',
+    padding: 8,
+    borderRadius: 20,
+    backgroundColor: "#38B6FF",
+    alignItems:'center'
+  },
+  btnTextAlert:{
+      color:'#fff',
+      fontSize:'13',
+      fontWeight:'500'
+  },
+  card: {
+    backgroundColor: "#e8eaea",
+    height: "auto",
+    borderRadius: 8,
+    padding: 15,
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginVertical: 10,
+    marginHorizontal: 10,
+  },
+  viewInfo: {
+    width: "100%",
+  },
+  dataPost: {
+    fontSize: 13,
+    fontWeight: "300",
+    textAlign: "right",
+    marginVertical: 2,
+  },
+  titulo: {
+    fontSize: 18,
+    fontWeight: "700",
+    textTransform: "uppercase",
+    marginVertical: 5,
+  },
+  description: {
+    width: "100%",
+    marginBottom: 10,
+  },
+  fotoPerfil: {
+    width: 50,
+    height: 50,
+    borderRadius: 60,
+    borderWidth: 2,
+    borderColor: "#0e52B2",
+  },
+  ViewBtn: {
+    width: "80%",
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  button: {
+    width: "40%",
+    backgroundColor: "#38B6FF",
+    borderRadius: 25,
+    padding: 15,
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 18,
+    color: "#fff",
+    fontWeight: "800",
+    letterSpacing: 1,
+  },
+  status: {
+    color: "#38B6FF",
+    fontWeight: "bold",
+    fontSize: 18,
+    textDecorationLine: "underline",
+    textTransform: "uppercase",
+    marginBottom: 5,
+  },
+  boxImages: {
+    width: "100%",
+    padding: 5,
+  },
+  img: {
+    width: 70,
+    height: 70,
+    margin: 10,
+  },
 });
--- src/pages/minhaPubs/index.js
+++ src/pages/minhaPubs/index.js
@@ -1,80 +1,87 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 
 import { getAuth } from "firebase/auth";
 import { getDocs, query, collection, where } from "firebase/firestore";
-import db from '../../config/configFirebase';
+import db from "../../config/configFirebase";
 
-import MinhaPublicacao from '../../components/minhaPublicacao';
-import Backbutton from '../../components/Backbutton';
+import MinhaPublicacao from "../../components/minhaPublicacao";
+import Backbutton from "../../components/Backbutton";
 
 export default function MinhaPubs({ navigation }) {
-
   const [post, setPost] = useState([]);
 
   const fetchPublicacoes = async () => {
     const auth = getAuth();
     const user = auth.currentUser;
-    const q = query(collection(db, 'posts'), where("userId", "==", user.uid));
+    const q = query(collection(db, "posts"), where("userId", "==", user.uid));
     const querySnapshot = await getDocs(q);
-    const List = []
+    const List = [];
     querySnapshot.forEach((doc) => {
-      List.push({ ...doc.data(), id: doc.id, })
+      List.push({ ...doc.data(), id: doc.id });
       console.log(doc.id, "=>", doc.data());
     });
-    console.log(post)
-    return setPost(List)
-  }
+    console.log(post);
+    return setPost(List);
+  };
 
   useEffect(() => {
     fetchPublicacoes();
   }, []);
 
   return (
-
     <View style={styles.container}>
       <Backbutton onClick={() => navigation.goBack()} />
-      <Text style={styles.titulo}>
-        MINHAS PUBLICAÇÕES
-      </Text>
+      <Text style={styles.titulo}>MINHAS PUBLICAÇÕES</Text>
 
-      <FlatList
-        vertical={true}
-        data={post}
-        renderItem={({ item }) => (
-
-          <MinhaPublicacao
-            tipoAjuda={item.tipoAjuda}
-            sobreVoce={item.sobreVoce}
-            status={item.status}
-            dataHoraPost={item.dataHoraPost}
-            imgPost1={item.imgPost1}
-            imgPost2={item.imgPost2}
-            imgPost3={item.imgPost3}
-            navigation={navigation}
-          />
-        )}
-      />
-
-
+      {post ? (
+        <FlatList
+          vertical={true}
+          data={post}
+          ListEmptyComponent={ <Text style={styles.aviso}>Você não tem nunhuma publicação</Text>}
+          renderItem={({ item }) => (
+            <MinhaPublicacao
+              tipoAjuda={item.tipoAjuda}
+              sobreVoce={item.sobreVoce}
+              status={item.status}
+              dataHoraPost={item.dataHoraPost}
+              imgPost1={item.imgPost1}
+              imgPost2={item.imgPost2}
+              imgPost3={item.imgPost3}
+              idPost={item.id}
+              navigation={navigation}
+            />
+          )}
+        />
+      ) : (
+        <Text style={styles.aviso}>Você não tem nenhuma publicações</Text>
+      )}
     </View>
-
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff'
+    backgroundColor: "#fff",
   },
   titulo: {
-    alignSelf: 'center',
-    width: '90%',
+    alignSelf: "center",
+    width: "100%",
     fontSize: 40,
-    color: '#38b6ff',
+    color: "#38b6ff",
     marginTop: 30,
-    textAlign: 'center',
-    fontWeight: '700',
+    textAlign: "center",
+    fontWeight: "700",
     marginBottom: 50,
   },
-})
+  aviso:{
+    width:'100%',
+    fontSize: 20,
+    fontWeight: "700",
+    textTransform:'uppercase',
+    color: '#0e52B2',
+    marginTop:50,
+    textAlign:'center'
+  }
+});
