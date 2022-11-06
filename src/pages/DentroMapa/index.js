4ae46cbddaaf247505aff1c08096fb11c315894a
W3llz1n <wellintoh.silva.brito@gmail.com>
2022-11-06 18:59:01 -0300

    Ajustando: Dentro do mapa

--- src/pages/DentroMapa/index.js
+++ src/pages/DentroMapa/index.js
@@ -1,25 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ImageBackground, View, StyleSheet, TextInput } from 'react-native';
 import Backbutton from '../../components/Backbutton';
+import MapView from 'react-native-maps';
+import * as Location from 'expo-location';
 
 export default function DentroMapa({ navigation }) {
 
     const [input, setInput] = useState('');
     const [image, setImage] = useState('https://www.google.com/maps/d/thumbnail?mid=10Y7jwq7YSUDlYxhONy5GVrb8JfI');
 
+    const [location, setLocation] = useState(null);
+    const [errorMsg, setErrorMsg] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                setErrorMsg('Permission to access location was denied');
+                return;
+            }
+
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation({
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+            });
+            console.log(location)
+        })();
+    }, []);
+
+
     return (
         <View style={styles.container}>
             <Backbutton onClick={() => navigation.goBack()} />
-            <View style={styles.imgContainer}>
-                <ImageBackground source={{ uri: image }} resizeMode="cover" style={styles.img}>
-                    <TextInput
-                        style={styles.input}
-                        onChangeText={setInput}
-                        value={input}
-                        placeholder="Pesquisar por Bairro, Cidade ou Estado"
-                    />
-                </ImageBackground>
-            </View>
+            <MapView
+                style={styles.imgContainer}
+                initialRegion={location}
+                showsUserLocation={true}
+                zoomEnabled={true}
+                loadingEnabled={true}
+            >
+
+            </MapView>
+            <TextInput
+                style={styles.input}
+                onChangeText={setInput}
+                value={input}
+                placeholder="Pesquisar por Bairro, Cidade ou Estado"
+            />
 
         </View>
     );
@@ -33,13 +64,9 @@ const styles = StyleSheet.create({
     },
     imgContainer: {
         width: '90%',
-        height: '90%',
+        height: '70%',
         alignItems: 'center',
         marginTop: 20
-    },
-    img: {
-        width: '100%',
-        height: '100%'
     },
     input: {
         width: '90%',
