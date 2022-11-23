c94c0afb37b0f68f59dba80a4968a3e8fe35a5fe
W3llz1n <wellintoh.silva.brito@gmail.com>
2022-11-23 20:59:01 -0300

    Ajustando: estilo do mapa

--- src/pages/DentroMapa/index.js
+++ src/pages/DentroMapa/index.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { ImageBackground, View, StyleSheet, TextInput } from "react-native";
 import Backbutton from "../../components/Backbutton";
 import MapView from "react-native-maps";
+import { requireNativeComponent } from 'react-native';
 import * as Location from "expo-location";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import config from "../../config/configGoogleApi/config.json";
@@ -38,6 +39,7 @@ export default function DentroMapa({ navigation }) {
   return (
     <View style={styles.container}>
       <Backbutton onClick={() => navigation.goBack()} />
+
       <MapView
         style={styles.imgContainer}
         initialRegion={location}
@@ -45,57 +47,62 @@ export default function DentroMapa({ navigation }) {
         zoomEnabled={true}
         loadingEnabled={true}
       ></MapView>
-      <View style={styles.viewinput}>
-        <GooglePlacesAutocomplete
-          style={styles.input}  
-          placeholder="Busque uma institutição"
-          onPress={(data, details = null) => {
-            setDestination({
-              latitude: details.geometry.location.lat,
-              longitude: details.geometry.location.lng,
-              latitudeDelta: 0.0922,
-              longitudeDelta: 0.0421,
-            });
-            console.log(destination);
-          }}
-          query={{
-            key: config.googleApi,
-            language: "pt-br",
-          }}
-          enablePoweredByContainer={false}
-          fetchDetails={true}
-          styles={{ listView: { height: 100 } }}
-        />
-      </View>
 
-      {/* <TextInput
-        style={styles.input}
-        onChangeText={setInput}
-        value={input}
-        placeholder="Pesquisar por Bairro, Cidade ou Estado"
-      /> */}
+      <GooglePlacesAutocomplete
+        placeholder="Busque uma institutição"
+        minLength={2}
+        autoFocus={false}
+        onPress={(data, details = null) => {
+          setDestination({
+            latitude: details.geometry.location.lat,
+            longitude: details.geometry.location.lng,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          });
+          console.log(destination);
+        }}
+        query={{
+          key: config.googleApi,
+          language: "pt-br",
+        }}
+        enablePoweredByContainer={false}
+        fetchDetails={true}
+        styles={{
+          textInputContainer: {
+            width: '100%',
+            backgroundColor: 'transparent',
+            padding: 5,
+            alignItems: 'center',
+            zIndex: 1
+          },
+          textInput: {
+            backgroundColor: '#FFFFFF',
+            borderWidth: 2,
+            borderColor: 'rgb(14, 82, 178)',
+            height: 40,
+            borderRadius: 5,
+            paddingVertical: 5,
+            paddingHorizontal: 10,
+            fontSize: 15,
+          },
+          predefinedPlacesDescription: {
+            color: '#1faadb',
+          },
+
+        }}
+      />
+
     </View>
   );
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    width: "100%",
     backgroundColor: "#fff",
   },
   imgContainer: {
-    width: "100%",
-    height: "60%",
-    marginTop: 20,
+    width: '100%',
+    height: '70%',
+    marginTop: 10,
   },
-  viewinput: {
-    width: "100%",
-    height:'40%',
-    backgroundColor:'grey'
-  },
-  input:{
-      borderColor:'grey',
-      borderWidth:3
-  }
 });
