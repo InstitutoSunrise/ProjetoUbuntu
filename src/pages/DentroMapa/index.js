8edfd67fae24432c27bcdd56b0253ecb10631901
InacioFigueiredo13 <inaciofigueiredo13@gmail.com>
2022-11-27 18:14:35 -0300

    Atualizacoes no mapa

--- src/pages/DentroMapa/index.js
+++ src/pages/DentroMapa/index.js
@@ -1,19 +1,27 @@
-import React, { useState, useEffect } from "react";
-import { ImageBackground, View, StyleSheet, TextInput } from "react-native";
+import React, { useState, useEffect, useRef } from "react";
+import { ImageBackground, View, StyleSheet, TextInput, Text } from "react-native";
 import Backbutton from "../../components/Backbutton";
 import MapView from "react-native-maps";
-import { requireNativeComponent } from 'react-native';
 import * as Location from "expo-location";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import config from "../../config/configGoogleApi/config.json";
+import MapViewDirections from 'react-native-maps-directions';
+import { Dimensions } from 'react-native';
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 export default function DentroMapa({ navigation }) {
+
   const [input, setInput] = useState("");
   const [image, setImage] = useState(
     "https://www.google.com/maps/d/thumbnail?mid=10Y7jwq7YSUDlYxhONy5GVrb8JfI"
   );
 
+
+
   const [destination, setDestination] = useState(null);
+  const [distance, setDistance] = useState(null);
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
 
@@ -38,60 +46,77 @@ export default function DentroMapa({ navigation }) {
 
   return (
     <View style={styles.container}>
-      <Backbutton onClick={() => navigation.goBack()} />
-
       <MapView
         style={styles.imgContainer}
         initialRegion={location}
         showsUserLocation={true}
+        showsMyLocationButton={false}
         zoomEnabled={true}
         loadingEnabled={true}
-      ></MapView>
+      >
 
-      <GooglePlacesAutocomplete
-        placeholder="Busque uma institutição"
-        minLength={2}
-        autoFocus={false}
-        onPress={(data, details = null) => {
-          setDestination({
-            latitude: details.geometry.location.lat,
-            longitude: details.geometry.location.lng,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          });
-          console.log(destination);
-        }}
-        query={{
-          key: config.googleApi,
-          language: "pt-br",
-        }}
-        enablePoweredByContainer={false}
-        fetchDetails={true}
-        styles={{
-          textInputContainer: {
-            width: '100%',
-            backgroundColor: 'transparent',
-            padding: 5,
-            alignItems: 'center',
-            zIndex: 1
-          },
-          textInput: {
-            backgroundColor: '#FFFFFF',
-            borderWidth: 2,
-            borderColor: 'rgb(14, 82, 178)',
-            height: 40,
-            borderRadius: 5,
-            paddingVertical: 5,
-            paddingHorizontal: 10,
-            fontSize: 15,
-          },
-          predefinedPlacesDescription: {
-            color: '#1faadb',
-          },
+        {destination &&
+          <MapViewDirections
+            origin={location}
+            destination={destination}
+            apikey={config.googleApi}
+            strokeWidth={3}
+            strokeColor="#38b6ff"
+            onReady={result => {
+              setDistance(result.distance);
+            }}
+          />
+        }
+      </MapView>
 
-        }}
-      />
+      <View style={{ width: '85%', height: '50%', position: 'absolute', alignSelf: 'flex-start', marginTop: 16 }}>
+        <GooglePlacesAutocomplete
+          placeholder="Busque uma institutição"
+          minLength={2}
+          autoFocus={false}
+          onPress={(data, details = null) => {
+            console.log(data, details);
+            setDestination({
+              latitude: details.geometry.location.lat,
+              longitude: details.geometry.location.lng,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            });
+          }}
+          query={{
+            key: config.googleApi,
+            language: "pt-br",
+          }}
+          enablePoweredByContainer={false}
+          fetchDetails={true}
+          styles={{
+            container: {
+              flex: 1
+            },
+            textInputContainer: {
+              width: '100%',
+              backgroundColor: 'transparent',
+              padding: 5,
+              alignItems: 'center',
+              zIndex: 1
+            },
+            textInput: {
+              backgroundColor: '#FFFFFF',
+              borderWidth: 2,
+              borderColor: 'rgb(14, 82, 178)',
+              height: 40,
+              borderRadius: 5,
+              paddingVertical: 5,
+              paddingHorizontal: 10,
+              fontSize: 15,
+            },
+            predefinedPlacesDescription: {
+              color: '#1faadb',
+            },
 
+          }}
+        />
+      </View>
     </View>
   );
 }
@@ -101,8 +126,8 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
   },
   imgContainer: {
-    width: '100%',
-    height: '70%',
+    width: windowWidth,
+    height: windowHeight,
     marginTop: 10,
   },
 });
